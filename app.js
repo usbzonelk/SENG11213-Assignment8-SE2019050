fee1b9551e8b684394f2ff792a7d7da4eb673534
Bhashith Udawaththa <73486769+usbzonelk@users.noreply.github.com>
2021-11-14 19:45:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+document.querySelector(".product-1").addEventListener("click", function () {
+alert("Fresh Meat");
+})
+document.querySelector(".product-2").addEventListener("click", function () {
+    alert("Bananas");
+})
+document.querySelector(".product-3").addEventListener("click", function () {
+    alert("Guava");
+})
+document.querySelector(".product-4").addEventListener("click", function () {
+    alert("Water Melon");
+})
+document.querySelector(".product-5").addEventListener("click", function () {
+    alert("Grapes");
+})
+document.querySelector(".product-6").addEventListener("click", function () {
+    alert("Burger");
+})
+document.querySelector(".product-7").addEventListener("click", function () {
+    alert("Mango");
+})
+document.querySelector(".product-8").addEventListener("click", function () {
+    alert("Apples");
+})
